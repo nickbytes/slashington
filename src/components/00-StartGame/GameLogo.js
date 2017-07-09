bb2563a9265f98694fa35afb6ee1576a477b5922
Nick Beattie <nick@nickbytes.com>
2017-07-09 10:13:11 -0400

    🔪 those features in this version

---
+++ src/components/00-StartGame/GameLogo.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+const GameLogo = (props) => {
+  return (
+    <div>
+      <h1>Aurora Metropolis</h1>
+    </div>
+  )
+}
+
+export default GameLogo
---
+++ src/components/00-StartGame/StartGame.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react'
+
+const StartGame = (props) => {
+  return (
+    <div>
+      <p>Aurora Metropolis is a semi-fictional interactive story.</p>
+      <Link>Begin</Link>
+    </div>
+  )
+}
+
+export default StartGame
