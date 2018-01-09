04285503093bc8f9aef5841f3cb74bb64afbb380
Nick Beattie <nick@nickbytes.com>
2018-01-09 18:37:12 -0500

    lil tasks

---
+++ src/components/PlainButton/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const PlainButton = styled.button`
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  line-height: inherit;
+  border: none;
+  border-bottom: 2px solid black;
+  background-color: transparent;
+  cursor: pointer;
+  padding: 0 1px;
+`;
+
+export default PlainButton;
