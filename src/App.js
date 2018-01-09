d906a33eb87f27df64f43940b166094fb80c2f79
Nick Beattie <nick@nickbytes.com>
2018-01-09 10:36:03 -0500

    safety push

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import GoodMessengerScene from "./pages/Good-Messenger";
 import LeavingSlashington from "./pages/Slashington";
 import StartGame from "./components/StartGame/StartGame";
 import TheCouncil from "./pages/The-Council";
-import Train from "./pages/A-Train-to-Not-Washington";
 import newInitialState from "./store/newInitialState";
 import s01eWork from "./pages/NewPages/s01eWork";
+import s02eTrain from "./pages/NewPages/s02eTrain";
 
 const App = props => (
   <div>
     <Route exact path="/" component={StartGame} />
     <Route path="/s01eWork" component={s01eWork} />
+    <Route path="/s02eTrain" component={s02eTrain} />
 
-    <Route path="/A-Train-to-Not-Washington" component={Train} />
     <Route path="/The-Council" component={TheCouncil} />
     <Route path="/Slashington" component={LeavingSlashington} />
     <Route path="/Birthday" component={Birthday} />
