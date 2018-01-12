42eccb0d7adb605d3630d596c4f879152613776c
Nick Beattie <nick@nickbytes.com>
2018-01-12 11:07:46 -0500

    some cleanup, this is exciting

---
+++ src/pages/NewPages/credits/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const credits = () => (
+  <div>
+    <h3>
+      This game was able to be made thanks to these humans, inspirations, and
+      open source software.
+    </h3>
+    <ul>
+      <li>LG</li>
+      <li>
+        My real brother Alex, who did the voiceover for the voicemail you
+        received.
+      </li>
+      <li>Commander Keen</li>
+      <li>Seers' Catalogue</li>
+      <li>Fantom</li>
+      <li>Night in the Woods</li>
+      <li>Kentucky Route Zero</li>
+      <li>Choose Your Own Adventure</li> <li>Create-react-app</li>
+      <li>React</li>
+      <li>React Router</li>
+      <li>Styled-Components</li>
+      <li>Rebase</li>
+      <li>Refunk</li>
+      <li>Atom</li>
+      <li>VS Code</li>
+    </ul>
+  </div>
+);
+
+export default credits;
