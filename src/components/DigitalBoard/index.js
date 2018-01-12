2d73152cc44e97f9e87b747f7a9e880e24ed6fa7
Nick Beattie <nick@nickbytes.com>
2018-01-12 18:13:47 -0500

    i just invented time, this is crazy

--- src/components/DigitalBoard/index.js
+++ src/components/DigitalBoard/index.js
@@ -15,68 +15,187 @@ const InnerContainer = styled.div`
 `;
 
 const Header = styled.div`
-  margin: 0;
+  margin: 0 0 45px;
 `;
 
 const Title = styled.h1`
-  font-family: "Source Sans Pro", courier;
+  font-family: Arial;
   font-size: 20px;
   font-weight: normal;
   line-height: 22px;
 `;
 
 const Table = styled(FlexContainer)`
-  border-bottom: 1px solid yellow;
+  margin: 0;
 `;
 
 const TimeColumn = styled(FlexContainer)`
   margin: 0;
+  font-family: Arial;
   flex-grow: 1;
 `;
 
 const DestionationColumn = styled(FlexContainer)`
   margin: 0;
+  font-family: Arial;
   flex-grow: 1;
 `;
 
 const TrackColumn = styled(FlexContainer)`
   margin: 0;
+  font-family: Arial;
   flex-grow: 1;
 `;
 
 const SubHead = styled.h2`
-  margin: 0;
+  margin: 0 0 10px;
   font-weight: normal;
   font-size: 18px;
   line-height: 19px;
+  border-bottom: 2px solid #e6c16e;
+`;
+
+const NormalRow = styled.h4`
+  font-size: 16px;
+  line-height: 18px;
+  font-weight: normal;
+  margin: 0 0 5px;
 `;
 
 class DigitalBoard extends Component {
   state = {
-    fakeTimes: []
+    fakeBoard: [
+      {
+        id: 1,
+        fakeTime: "4:32",
+        fakeSunset: "am",
+        fakeDestination: "Something Capitalized",
+        fakeTrack: "02"
+      },
+      {
+        id: 2,
+        fakeTime: "5:32",
+        fakeSunset: "am",
+        fakeDestination: "City Oz",
+        fakeTrack: "02"
+      },
+      {
+        id: 3,
+        fakeTime: "6:32",
+        fakeSunset: "am",
+        fakeDestination: "Some Land",
+        fakeTrack: "02"
+      },
+      {
+        id: 4,
+        fakeTime: "7:32",
+        fakeSunset: "am",
+        fakeDestination: "What Ever ",
+        fakeTrack: "02"
+      }
+    ]
+  };
+  componentDidMount() {
+    console.log("this mounted");
+    console.log();
+    this.scramble();
+  }
+
+  componentWillUnmount() {
+    console.log("imma need to unmount the timer here");
+  }
+
+  randomTime = () => {
+    const hr = Math.floor(Math.random() * 24);
+    const min = Math.floor(Math.random() * 59);
+    return `${hr < 10 ? `0${hr}` : `${hr}`}:${min < 10 ? `0${min}` : `${min}`}`;
+  };
+
+  randomCity = () => {
+    const p = Math.random();
+    if (p > 0.5) {
+      // one word
+      return Math.random()
+        .toString(36)
+        .substring(3, 9);
+    } else {
+      // two words
+      return `${Math.random()
+        .toString(36)
+        .substring(3, 8)} ${Math.random()
+        .toString(36)
+        .substring(3, 13)}`;
+    }
+  };
+
+  randomTrack = () => {
+    return `${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}`;
+  };
+
+  scramble = () => {
+    console.log("scramble");
+    this.setState({
+      fakeBoard: [
+        {
+          id: 1,
+          fakeTime: this.randomTime(),
+          fakeDestination: this.randomCity(),
+          fakeTrack: this.randomTrack()
+        },
+        {
+          id: 2,
+          fakeTime: this.randomTime(),
+          fakeDestination: this.randomCity(),
+          fakeTrack: this.randomTrack()
+        },
+        {
+          id: 3,
+          fakeTime: this.randomTime(),
+          fakeDestination: this.randomCity(),
+          fakeTrack: this.randomTrack()
+        },
+        {
+          id: 4,
+          fakeTime: this.randomTime(),
+          fakeDestination: this.randomCity(),
+          fakeTrack: this.randomTrack()
+        }
+      ]
+    });
   };
   render() {
     return (
       <OuterContainer>
         <InnerContainer>
           <Header>
-            <Title>Now Arriving @ Slashington</Title>
+            <Title>Slashington Arrivals</Title>
           </Header>
 
           <Table justifyCenter contentSpaceAround>
-            <TimeColumn>
+            <TimeColumn directionColumn>
               <SubHead>Time</SubHead>
-              <h2>5:00am</h2>
+              <NormalRow>5:32</NormalRow>
+              {this.state.fakeBoard.map(fakeItem => (
+                <NormalRow key={fakeItem.id}>{fakeItem.fakeTime}</NormalRow>
+              ))}
             </TimeColumn>
 
-            <DestionationColumn>
+            <DestionationColumn directionColumn>
               <SubHead>Destionation</SubHead>
-              <h2>Union Station (DC)</h2>
+              <NormalRow>Union Station (DC)</NormalRow>
+              {this.state.fakeBoard.map(fakeItem => (
+                <NormalRow key={fakeItem.id}>
+                  {fakeItem.fakeDestination}
+                </NormalRow>
+              ))}
             </DestionationColumn>
 
-            <TrackColumn>
+            <TrackColumn directionColumn>
               <SubHead>Track</SubHead>
-              <h2>01</h2>
+              <NormalRow>01</NormalRow>
+              {this.state.fakeBoard.map(fakeItem => (
+                <NormalRow key={fakeItem.id}>{fakeItem.fakeTrack}</NormalRow>
+              ))}
             </TrackColumn>
           </Table>
         </InnerContainer>
