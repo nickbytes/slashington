6a5b8f985f1b5282b70d6052d50c591162d41865
Nick Beattie <nick@nickbytes.com>
2018-01-21 17:06:22 -0500

    lil dev tool for links

--- src/components/DemoMode/index.js
+++ src/components/DemoMode/index.js
@@ -3,24 +3,37 @@ import React from "react";
 
 import styled from "styled-components";
 
-const container = styled.div`
+const Container = styled.div`
   position: absolute;
   bottom: 0;
   right: 0;
   background-color: rgba(225, 88, 70, 0.5);
   border: 1px solid rgba(225, 88, 70, 1);
+  width: 175px;
+  padding: 3px;
+`;
+
+const DevLink = styled(Link)`
+  display: block;
+`;
+
+const DevTitle = styled.h1`
+  font-size: 10px;
+  line-height: 10px;
+  margin: 0;
 `;
 
 const DemoMode = () => (
-  <div>
-    <Link to={`/s01eWork`}>s01eWork</Link>
-    <Link to={`/s02eTrain`}>s02eTrain</Link>
-    <Link to={`/s03eSlashington`}>s03eSlashington</Link>
-    <Link to={`/s04eTheCouncil`}>s04eTheCouncil</Link>
-    <Link to={`/s05eSlashing2Washington`}>s05eSlashing2Washington</Link>
-    <Link to={`/s06eBirthdayParty`}>s06eBirthdayParty</Link>
-    <Link to={`/s07eGoodMessenger`}>s07eGoodMessenger</Link>
-  </div>
+  <Container>
+    <DevTitle>devtool</DevTitle>
+    <DevLink to={`/s01eWork`}>s01eWork</DevLink>
+    <DevLink to={`/s02eTrain`}>s02eTrain</DevLink>
+    <DevLink to={`/s03eSlashington`}>s03eSlashington</DevLink>
+    <DevLink to={`/s04eTheCouncil`}>s04eTheCouncil</DevLink>
+    <DevLink to={`/s05eSlashing2Washington`}>s05eSlashing2Washington</DevLink>
+    <DevLink to={`/s06eBirthdayParty`}>s06eBirthdayParty</DevLink>
+    <DevLink to={`/s07eGoodMessenger`}>s07eGoodMessenger</DevLink>
+  </Container>
 );
 
 export default DemoMode;
