b19fe04944c1f06ae15ba3939072fbd672b47a80
Nick Beattie <nick@nickbytes.com>
2018-02-09 17:03:52 -0500

    just commit cause whatever

---
+++ src/components/Leaves/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Leaves = () => (
+  <div>
+    <h1>Leaves graphic here.</h1>
+  </div>
+);
+
+export default Leaves;
--- src/pages/s7/index.js
+++ src/pages/s7/index.js
@@ -2,6 +2,7 @@ import { connect } from "funcup";
 import React from "react";
 
 import DemoMode from "../../components/DemoMode";
+import Leaves from "../../components/Leaves";
 import MainText from "../../components/MainText";
 import SimpleScene from "../../components/SimpleScene";
 import UserQuote from "../../components/UserQuote";
@@ -10,18 +11,31 @@ const s7 = props => (
   <div>
     <DemoMode />
     <SimpleScene isVisible={props.c}>
-      <MainText>This chair is comfortable.</MainText>
+      <Leaves />
+    </SimpleScene>
 
+    <SimpleScene>
+      <Leaves />
+      <MainText>
+        Wow, you must have dozed off a little bit there... what a week.
+      </MainText>
+      <MainText>
+        You survey the backyard. Teenagers running around, parents chatting, the
+        sublte hum of Nickelback. You sit up a little bit in the lawnchair.
+      </MainText>
+    </SimpleScene>
+
+    <SimpleScene>
       <UserQuote>
-        It's good having you down for the weekend, can't tell you how much it
-        means to us. And we don't mean to pester you about work, you know we're
-        just worried.
+        Have a bit of a nap there? They've really got you working hard.
       </UserQuote>
 
-      <MainText>How did you get in the backyard?</MainText>
-      <UserQuote />
+      <MainText>
+        Alex hands you a cold creme soda. Damn, you love creme soda.
+      </MainText>
+
+      <UserQuote>Thanks. Yea, I guess so.</UserQuote>
     </SimpleScene>
-    <SimpleScene />
   </div>
 );
 
