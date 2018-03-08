6ffc338866bae245ed01463efcea98d6986347cb
Nick Beattie <nick@nickbytes.com>
2018-03-08 00:05:18 -0500

    chapter next bigger

--- src/components/ChapterUtils/index.js
+++ src/components/ChapterUtils/index.js
@@ -34,8 +34,8 @@ const NextButtonStyle = styled.button`
   font-family: ${T_FF};
   color: #fff;
   font-weight: normal;
-  font-size: 26px;
-  line-height: 26px;
+  font-size: 38px;
+  line-height: 38px;
   padding: 20px;
   margin-left: -20px;
   cursor: pointer;
